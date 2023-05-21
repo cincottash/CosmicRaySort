5f58aae2f53c82d82df0184badece9a99847cf73
Shane Cincotta <cincotta@udel.edu>
2023-05-21 11:13:34 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def miracleSort(unsortedList):
+  while(unsortedList != sorted(unsortedList)):
+    print('Waiting for cosmic ray...\n)
+          
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
