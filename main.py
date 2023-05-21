799b0b7cf7700b4be97fcfca25a7b3f2b669073a
Shane Cincotta <cincotta@udel.edu>
2023-05-21 11:20:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import time
+import random
+
 def miracleSort(unsortedList):
+  print(f'Unsorted list:{unsortedList}\n')
   while(unsortedList != sorted(unsortedList)):
-    print('Waiting for cosmic ray...\n)
-          
+    print('Waiting for cosmic ray...\n')
+    time.sleep(1)
+
+  print(f'A cosmic ray has sorted the list!\nSorted list:{unsortedList}')
+
 def main():
-  pass
+  myList = [random.randint(0, 10) for x in range(10)]
+  miracleSort(myList)
 
 if __name__ == '__main__':
   main()
